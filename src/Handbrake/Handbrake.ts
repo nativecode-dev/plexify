4703b3e3eb13d12c2329997e22f31c7ed2eff2eb
NativeCode <opensource@nativecode.com>
2019-03-25 23:14:09 -0400

    handbrake

--- src/Handbrake/Handbrake.ts
+++ src/Handbrake/Handbrake.ts
@@ -1,4 +1,12 @@
 import { spawn, HandbrakeOptions } from 'handbrake-js'
+import { HandbrakeEvent } from './HandbrakeEvent'
+import { Logger } from '../Logging'
+
+export interface EncodeResults {
+  filename: string
+  output: string[]
+  success: boolean
+}
 
 const DefaultOptions: Partial<HandbrakeOptions> = {
   'all-audio': true,
@@ -13,5 +21,46 @@ export class Handbrake {
 
   constructor(handbrakeOptions?: Partial<HandbrakeOptions>) {
     this.options = { ...DefaultOptions, ...handbrakeOptions } as HandbrakeOptions
+  }
+
+  encode(source: string, target: string): Promise<EncodeResults> {
+    const results: EncodeResults = {
+      filename: source,
+      output: [],
+      success: false,
+    }
+
+    return new Promise((resolve, reject) => {
+      let errored = false
+      spawn(this.options)
+        .on(HandbrakeEvent.Cancelled, () => {
+          Logger.info(`Encoding ${source} cancelled`)
+          results.success = false
+          errored = true
+          reject(results)
+        })
+        .on(HandbrakeEvent.Complete, () => {
+          Logger.info(`Encoding ${source} completed`)
+          results.success = errored
+          resolve(results)
+        })
+        .on(HandbrakeEvent.Error, (error: Error) => {
+          Logger.info(`Encoding ${source} errored: ${error}`)
+          results.success = false
+          errored = true
+          reject(error)
+        })
+        .on(HandbrakeEvent.Output, (buffer: Buffer) => {
+          const lines = this.split(buffer)
+          results.output = results.output.concat(...lines)
+        })
+    })
+  }
+
+  private split(buffer: Buffer): string[] {
+    return buffer
+      .toString()
+      .replace('\r', '')
+      .split('\n')
   }
 }
